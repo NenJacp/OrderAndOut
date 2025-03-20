c0a22b367aa06604b33192dc16da4915e7368c4f
NenJacp <cruzjorge.059.yuc@gmail.com>
2025-03-20 09:31:03 -0600

    fix(customer): eliminado

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import kioskRouter from './src/modules/Kiosk/kiosk.routes.js'; // Importar las r
 import orderRouter from './src/modules/Order/order.routes.js'; // Importar las rutas de órdenes
 import productRouter from './src/modules/Product/product.routes.js'; // Importar las rutas de productos
 import categoriesRouter from './src/modules/Category/category.routes.js'; // Importar las rutas de categorías
-import customerRouter from './src/modules/Customer/customer.routes.js'; // Importar las rutas de clientes
 import stripeRouter from './src/modules/Stripe/stripe.routes.js'; // Importar las rutas de Stripe
 import cleanupJob from './src/config/cleanup.js';
 import cors from 'cors';
@@ -33,7 +32,6 @@ app.use('/api/kiosks', kioskRouter); // Usar la instancia de la aplicación
 app.use('/api/orders', orderRouter); // Usar la instancia de la aplicación
 app.use('/api/products', productRouter); // Usar la instancia de la aplicación
 app.use('/api/categories', categoriesRouter); // Usar la instancia de la aplicación
-app.use('/api/customers', customerRouter); // Usar la instancia de la aplicación para clientes
 app.use('/api/stripe', stripeRouter); // Usar la instancia de la aplicación para Stripe
 
 // Iniciar limpieza automática
